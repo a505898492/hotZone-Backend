6a21491b8dea1926c25514eb932185434a1cad57
Jong <kau96kim@gmail.com>
2020-11-02 23:33:45 +0900

    change db

--- hotZone/settings.py
+++ hotZone/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'hotZone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hotzone',
+        'USER': 'kau96kim',
+        'PASSWORD': 'jong107493!',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
